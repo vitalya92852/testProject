d2641ac5e12c19b706b32e3a3f44ad6c7d2a27a6
Виталий <vitalya92852@gmail.com>
2024-07-31 11:00:29 +0300

    fixed student filter

--- src/main/kotlin/org/example/project/dal/POJO/AddressPOJO.kt
+++ src/main/kotlin/org/example/project/dal/POJO/AddressPOJO.kt
@@ -1,6 +1,6 @@
 package org.example.project.dal.POJO
 
-data class CityPOJO(
+data class AddressPOJO(
     var city:String?=null,
     var country:String?=null
 )
--- src/main/resources/db/changelog/changeset/30-07-2024-14-24-add-column-city-to-student.sql
+++ src/main/resources/db/changelog/changeset/30-07-2024-14-24-add-column-city-to-student.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 --changeset vitaliy:add-column-city-to-student
 ALTER TABLE student
-ADD COLUMN city jsonb;
+ADD COLUMN address jsonb;
+
 
 
